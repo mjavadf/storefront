3eb31a63c77553075d4fabc84479680be1a6112d
mjavadf <javad.me1376@gmail.com>
2023-06-13 16:00:53 +0200

    add zip field to address model

--- store/models.py
+++ store/models.py
@@ -66,6 +66,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=10)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 class Cart(models.Model):
