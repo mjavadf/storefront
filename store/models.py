27aff14f219af0dc3fa7502094434eacfdc81f93
mjavadf <javad.me1376@gmail.com>
2023-06-14 17:15:20 +0200

    rename mail field to email

--- store/models.py
+++ store/models.py
@@ -34,7 +34,7 @@ class Customer(models.Model):
     
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    mail = models.EmailField(unique=True)
+    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1,
