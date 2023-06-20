e59622a24ac5ad02bd783096f36440146cba0695
mjavadf <javad.me1376@gmail.com>
2023-06-20 18:44:20 +0200

    complete admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,17 +1,46 @@
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from django.db.models import Count
+
+from tags.models import TaggedItem
 from . import models
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = "Inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [("<10", "Low")]
+
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+
+
+class TagInline(GenericTabularInline):
+    model = TaggedItem
+    extra = 1
+    autocomplete_fields = ['tag']
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = [
-        "title",
-        "unit_price",
-        "inventory_status",
-    ]
+    prepopulated_fields = {"slug": ["title"]}
+    autocomplete_fields = ["collection"]
+    actions = ["clear_inventory"]
+    list_display = ["title", "unit_price", "inventory_status", "collection"]
     list_editable = ["unit_price"]
     list_per_page = 10
+    list_filter = ["last_update", "collection", InventoryFilter]
     ordering = ["inventory"]
+    search_fields = ["title"]
+    inlines = [TagInline]
 
     @admin.display(ordering="inventory")
     def inventory_status(self, product):
@@ -19,13 +48,38 @@ class ProductAdmin(admin.ModelAdmin):
             return "LOW"
         return "OK"
 
+    @admin.action(description="Clear Inventory")
+    def clear_inventory(self, request, queryset):
+        update_count = queryset.update(inventory=0)
+        self.message_user(request, f"{update_count} products were successfully updated")
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "membership"]
+    list_display = ["first_name", "last_name", "membership", "orders_count"]
     list_editable = ["membership"]
     ordering = ["first_name", "last_name"]
     list_per_page = 10
+    search_fields = ["first_name__istartswith", "last_name__istartswith"]
+
+    @admin.display(ordering="orders_count")
+    def orders_count(self, customer):
+        url = (
+            reverse("admin:store_order_changelist")
+            + "?"
+            + urlencode({"customer__id": customer.id})
+        )
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(orders_count=Count("order"))
+
+
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    autocomplete_fields = ["product"]
+    extra = 1
+    min_num = 1
 
 
 @admin.register(models.Order)
@@ -34,6 +88,26 @@ class OrderAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_editable = ["payment_status"]
     list_select_related = ["customer"]
+    inlines = [OrderItemInline]
 
     def customer_full_name(self, order):
         return f"{order.customer.first_name} {order.customer.last_name}"
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title", "products_count"]
+    search_fields = ["title"]
+
+    @admin.display(ordering="products_count")
+    def products_count(self, collection):
+        # admin:app_model_page
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__id": collection.id})
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count("product"))
--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Promotion(models.Model):
@@ -25,7 +26,10 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(
+        max_digits=6, 
+        decimal_places=2,
+        validators=[MinValueValidator(1)])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
