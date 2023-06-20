2c99de54b0e42a8f8c496c9d8c1f735a624ff377
mjavadf <javad.me1376@gmail.com>
2023-06-20 20:51:31 +0200

    implement api view of products, product and collection

--- store/views.py
+++ store/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Collection, Product
+from .serializers import CollectionSerializer, ProductSerializer
+from store import serializers
 
-# Create your views here.
+
+@api_view()
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True, context={"request": request})
+    return Response(serializer.data)
+
+
+@api_view()
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product, context={"request": request})
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
