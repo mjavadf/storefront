c4abdb0d514285273fbd772b4be2e043331c0767
mjavadf <javad.me1376@gmail.com>
2023-06-22 17:13:18 +0200

    create review model

--- store/models.py
+++ store/models.py
@@ -32,7 +32,7 @@ class Product(models.Model):
         validators=[MinValueValidator(1)])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT, related_name='products')
     promotions = models.ManyToManyField(Promotion)
     
     def __str__(self):
@@ -123,3 +123,9 @@ class CartItem(models.Model):
     
     def __str__(self):
         return f'{self.quantity} X {self.product}'
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
