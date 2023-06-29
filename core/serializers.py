614a4b71f28f32c27a5ebfe2fc2a7ae75328a7dd
mjavadf <javad.me1376@gmail.com>
2023-06-29 13:11:23 +0200

    Create custom serializer for current_user djoser

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,12 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerialize
+from djoser.serializers import UserSerializer as BaseUserSerializer
 
 
 class UserCreateSerializer(BaseUserCreateSerialize):
     class Meta(BaseUserCreateSerialize.Meta):
         fields = ["id", "username", "password", "email", "first_name", "last_name"]
+        
+        
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = fields = ["id", "username", "email", "first_name", "last_name"]
--- storefront/settings.py
+++ storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import environ
 
@@ -153,14 +154,17 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-}
-
 AUTH_USER_MODEL = "core.User"
 
 DJOSER = {
-    'SERIALIZERS': {
-        'user_create': 'core.serializers.UserCreateSerializer'
+    "SERIALIZERS": {
+        "user_create": "core.serializers.UserCreateSerializer",
+        "current_user": "core.serializers.UserSerializer",
     }
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
