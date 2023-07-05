c9f163a1b31f3aeb808f8a2f4db29d702165001b
mjavadf <javad.me1376@gmail.com>
2023-07-05 18:13:09 +0200

    Show images inside products

--- store/admin.py
+++ store/admin.py
@@ -26,7 +26,21 @@ class InventoryFilter(admin.SimpleListFilter):
 class TagInline(GenericTabularInline):
     model = TaggedItem
     extra = 1
-    autocomplete_fields = ['tag']
+    autocomplete_fields = ["tag"]
+
+
+class ProductImageInline(admin.TabularInline):
+    model = models.ProductImage
+    readonly_fields = ["thumbnail"]
+
+    def thumbnail(self, instance):
+        if instance.image.name != "":
+            return format_html(
+                f"""
+        <img src="{instance.image.url}" class="thumbnail"/>
+        """
+            )
+        return ""
 
 
 @admin.register(models.Product)
@@ -38,9 +52,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ["unit_price"]
     list_per_page = 10
     list_filter = ["last_update", "collection", InventoryFilter]
-    ordering = ["inventory"]
+    ordering = ["inventory", "id"]
     search_fields = ["title"]
-    inlines = [TagInline]
+    inlines = [TagInline, ProductImageInline]
 
     @admin.display(ordering="inventory")
     def inventory_status(self, product):
@@ -53,12 +67,15 @@ class ProductAdmin(admin.ModelAdmin):
         update_count = queryset.update(inventory=0)
         self.message_user(request, f"{update_count} products were successfully updated")
 
+    class Media:
+        css = {"all": ["store/styles.css"]}
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership", "orders_count"]
     list_editable = ["membership"]
-    list_select_related = ['user']
+    list_select_related = ["user"]
     ordering = ["user__first_name", "user__last_name"]
     list_per_page = 10
     search_fields = ["first_name__istartswith", "last_name__istartswith"]
