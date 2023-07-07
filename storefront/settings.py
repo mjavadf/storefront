595af16dbdde5f591d387316d1a44fa08220f0d5
mjavadf <javad.me1376@gmail.com>
2023-07-07 15:36:22 +0200

    Use redis as cache backend

--- storefront/settings.py
+++ storefront/settings.py
@@ -192,3 +192,13 @@ CELERY_BEAT_SCHEDULE = {
         # you can also use args or kwargs here
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
